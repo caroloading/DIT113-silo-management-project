9f841fb4a8ad81601f3de0c2c4ec802868554b32
Szymon Witt <guswitsz@student.gu.se>
2024-04-08 14:31:01 +0200

    chore: add spring boot project skeleton

---
+++ silo-web-app/src/main/java/group1/silowebapp/SiloWebAppApplication.java
@@ -0,0 +1,29 @@
+package group1.silowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+@RestController
+public class SiloWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiloWebAppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ silo-web-app/src/test/java/group1/silowebapp/SiloWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group1.silowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiloWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
