8ecb1dfb1f321207a3a33712cb49e4c20311164c
Caroline Nathalie Jeanne Grand-Clement <gusgrandca@student.gu.se>
2024-04-17 13:47:33 +0200

    Parse the received data as JsonNode for easier manipulation

--- silo-web-app/src/main/java/group1/silowebapp/mqtt/SensorsMessageHandler.java
+++ silo-web-app/src/main/java/group1/silowebapp/mqtt/SensorsMessageHandler.java
@@ -5,11 +5,16 @@ import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.MessagingException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import group1.silowebapp.model.EntityFactory;
 import group1.silowebapp.repository.GrainHeightRepository;
 import group1.silowebapp.repository.HumidityRepository;
 import group1.silowebapp.repository.SiloRepository;
 import group1.silowebapp.repository.TemperatureRepository;
+
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -35,21 +40,28 @@ public class SensorsMessageHandler implements MessageHandler {
         MessageHeaders headers = message.getHeaders();                  //gets meta-info about message
         headers.forEach((k, v) -> System.out.println(k + "=" + v));
 		System.out.println(message.getPayload());
-
-        String content = message.getPayload().toString();
-        String[] splitContent = content.split("/");
-        Double reading = Double.valueOf(splitContent[0]);
-        String dateTime = splitContent[1];
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = null;
+        
+        try {
+            node = mapper.readTree(message.getPayload().toString());
+        } catch (IOException ex){
+            System.out.printf("Could not parse to JsonNode. Reason: %s%n", ex);
+            return;
+        }
+        
+        Double value = node.get("value").asDouble();
+        String dateTime = node.get("dateTime").asText();
 
         switch(type){
             case "Humidity":
-                humiRepo.save(EntityFactory.createHumidity(reading, dateTime, siloRepository.findById(1L)));
+                humiRepo.save(EntityFactory.createHumidity(value, dateTime, siloRepository.findById(1L)));
                 break;
             case "Temperature":
-                tempRepo.save(EntityFactory.createTemperature(reading, dateTime, siloRepository.findById(1L)));
+                tempRepo.save(EntityFactory.createTemperature(value, dateTime, siloRepository.findById(1L)));
                 break;
             case "GrainHeight":
-                quantityRepo.save(EntityFactory.createGrainHeight(reading, dateTime, siloRepository.findById(1L)));
+                quantityRepo.save(EntityFactory.createGrainHeight(value, dateTime, siloRepository.findById(1L)));
                 break;
         }
     }
