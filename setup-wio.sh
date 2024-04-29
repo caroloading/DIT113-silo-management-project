29daa55f1d9291d071aa56017ca2e783484d4ff4
Charles Jarju <gusjarjch@student.gu.se>
2024-04-29 12:26:56 +0000

    Update setup-wio.sh

--- setup-wio.sh
+++ setup-wio.sh
@@ -15,19 +15,21 @@ arduino-cli core update-index --config-file .arduino-cli.yaml
 arduino-cli core install Seeeduino:samd --config-file .arduino-cli.yaml
 
 # Install 'native' packages (libraries that do not come with the core)
-arduino-cli lib install "Seeed Arduino rpcWiFi@1.0.7"
 arduino-cli lib install "Seeed Arduino RTC@2.0.0"
+arduino-cli lib install "Seeed Arduino rpcWiFi@1.0.7"
+arduino-cli lib install "Grove Temperature And Humidity Sensor@2.0.2"
+arduino-cli lib install "Grove Ultrasonic Ranger@1.0.1"
+arduino-cli lib install "Grove LED Bar@1.0.0"
 arduino-cli lib install "PubSubClient@2.8"
 arduino-cli lib install "NTPClient@3.2.1"
 arduino-cli lib install "ArduinoJson@7.0.4"
-#arduino-cli lib install "Grove LED Bar@1.0.0"
 
-cd ~
+#cd ~
 
 # Install 'third-party' packages / libraries: find proper location and 'git clone'
-apt-get install git -y
-cd `arduino-cli config dump | grep user | sed 's/.*\ //'`/libraries
-git clone https://github.com/Seeed-Studio/Grove_LED_Bar.git
-git clone https://github.com/Seeed-Studio/Grove_Temperature_And_Humidity_Sensor.git
+#apt-get install git -y
+#cd `arduino-cli config dump | grep user | sed 's/.*\ //'`/libraries
+#git clone https://github.com/Seeed-Studio/Grove_LED_Bar.git
+#git clone https://github.com/Seeed-Studio/Grove_Temperature_And_Humidity_Sensor.git
 #git clone https://github.com/Seeed-Studio/Seeed_Arduino_RTC.git
-git clone https://github.com/Seeed-Studio/Seeed_Arduino_UltrasonicRanger.git
+#git clone https://github.com/Seeed-Studio/Seeed_Arduino_UltrasonicRanger.git
