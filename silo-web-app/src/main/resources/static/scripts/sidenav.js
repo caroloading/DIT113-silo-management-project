624171d3f9c8c5da5d3eacf641de0dd42ea63cba
Szymon Witt <guswitsz@student.gu.se>
2024-05-01 14:37:25 +0200

    (#14) fix: sidenav active element is not visible on mobile resolution

--- silo-web-app/src/main/resources/static/scripts/sidenav.js
+++ silo-web-app/src/main/resources/static/scripts/sidenav.js
@@ -1,16 +1,18 @@
-const sidenavLinks = document.getElementById("sidenav-links");
+const sidenavLinks = document.querySelectorAll(".sidenav-links");
 const activeNavItemClassName = "c-nav-item-active";
 
 const setActiveNavItem = () => {
     let url = window.location.href;
-    sidenavLinks.querySelectorAll("li").forEach((li) => {
-        let link = li.querySelector("a").href;
+    sidenavLinks.forEach((sidenav) => {
+        sidenav.querySelectorAll("li").forEach((li) => {
+            let link = li.querySelector("a").href;
 
-        if (link === url && !li.classList.contains(activeNavItemClassName)) {
-            li.classList.add(activeNavItemClassName);
-        } else {
-            li.classList.remove(activeNavItemClassName);
-        }
+            if (link === url && !li.classList.contains(activeNavItemClassName)) {
+                li.classList.add(activeNavItemClassName);
+            } else {
+                li.classList.remove(activeNavItemClassName);
+            }
+        })
     })
 }
 
