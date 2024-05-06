6271ed2764503f1923abb827ca9f6e89a0c2beb8
Szymon Witt <guswitsz@student.gu.se>
2024-05-06 12:22:32 +0200

    (#30) feat: add all silos page

---
+++ silo-web-app/src/main/java/group1/silowebapp/controller/SiloController.java
@@ -0,0 +1,28 @@
+package group1.silowebapp.controller;
+
+import group1.silowebapp.repository.SiloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.Year;
+
+@Controller
+public class SiloController {
+    @Autowired
+    private SiloRepository siloRepository;
+
+    @GetMapping("/silos")
+    public String getSilos(Model silos) {
+        silos.addAttribute("silos", siloRepository.findAll());
+        silos.addAttribute("thisYear", Year.now());
+        return "silos";
+    }
+
+    @GetMapping("/silos/{id}")
+    public String getSilo(@PathVariable("id") String siloId) {
+        return "";
+    }
+}
